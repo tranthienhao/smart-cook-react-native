420f225d796abc698a01b58cbad7edf37f8674f5
Pham Nhut <hong-quan.nguyen@ant-tech.eu>
2018-07-04 14:38:45 +0700

    Add MenuDetailScreen

--- App/Containers/MenuDetailScreen.js
+++ App/Containers/MenuDetailScreen.js
@@ -1,32 +1,49 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
-import { connect } from 'react-redux'
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
+import React, { Component } from "react";
+import { ScrollView, Text, Image, View } from "react-native";
+import { connect } from "react-redux";
 
 // Styles
-import styles from './Styles/MenuDetailScreenStyle'
+import styles from "./Styles/MenuDetailScreenStyle";
 
 class MenuDetailScreen extends Component {
-  render () {
+  render() {
+    const data = this.props.navigation.getParam("data");
     return (
       <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>MenuDetailScreen</Text>
-        </KeyboardAvoidingView>
+        <Image style={styles.image1} source={{ uri: data.FoodImage1 }} />
+        {data.FoodId2 ? (
+          <View style={styles.imageView}>
+            <Image style={styles.image} source={{ uri: data.FoodImage2 }} />
+            {data.FoodId3 ? (
+              <Image style={styles.image} source={{ uri: data.FoodImage3 }} />
+            ) : (
+              <View />
+            )}
+          </View>
+        ) : (
+          <View />
+        )}
+        <View style={styles.infoView}>
+          <Text style={styles.name}>{data.Name}</Text>
+          <Text style={styles.category}>{data.CategoryName}</Text>
+          <Text style={styles.cookingTime}>{data.CookingTime}</Text>
+          <Text style={styles.description}>{data.Description}</Text>
+          <Text style={styles.note}>Bạn hãy ấn vào hình để xem công thức cụ thể cho từng món nhé!</Text>
+        </View>
       </ScrollView>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
+const mapStateToProps = state => {
+  return {};
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
+const mapDispatchToProps = dispatch => {
+  return {};
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(MenuDetailScreen)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MenuDetailScreen);
--- App/Containers/Styles/MenuDetailScreenStyle.js
+++ App/Containers/Styles/MenuDetailScreenStyle.js
@@ -1,6 +1,44 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
-
+import { Colors, Metrics } from '../../Themes'
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  image1: {
+    height: Metrics.screenHeight / 3,
+    width: Metrics.screenWidth,
+    margin: 2
+  },
+  image: {
+    flex: 1,
+    margin: 2
+  },
+  imageView: {
+    height: Metrics.screenHeight / 2.5,
+    width: Metrics.screenWidth,
+    flexDirection: 'row'
+  },
+  infoView: {
+    padding: Metrics.baseMargin,
+    backgroundColor: "white"
+  },
+  name: {
+    fontSize: 18,
+    color: Colors.accent,
+    fontFamily: "serif",
+    marginBottom: Metrics.baseMargin,
+    fontWeight: "bold"
+  },
+  category: {
+    fontStyle: "italic",
+    fontSize: 15,
+    color: Colors.accent
+  },
+  cookingTime: {
+    fontSize: 12,
+    alignSelf: "flex-end",
+    fontStyle: "italic"
+  },
+  note:{
+    color:'black',
+    fontStyle: 'italic',
+    marginTop: Metrics.baseMargin
+  }
+});
